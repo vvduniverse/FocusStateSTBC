9737ab5fbdd5c71840ddc26b818df9e2091ec94d
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-05 22:24:00 +0300

    Initial Commit

---
+++ FocusStateSTBC/FocusStateSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusStateSTBCApp.swift
+//  FocusStateSTBC
+//
+//  Created by Vahtee Boo on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FocusStateSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
