867a6a58fa84cccafac2db514363c8e44ad1e288
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-05 22:25:48 +0300

    done

--- FocusStateSTBC/ContentView.swift
+++ FocusStateSTBC/ContentView.swift
@@ -2,15 +2,84 @@
 //  ContentView.swift
 //  FocusStateSTBC
 //
-//  Created by Vahtee Boo on 05.04.2022.
+//  Created by Vahtee Boo on 21.03.2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum OnboardingField: Hashable {
+        case username
+        case password
+    }
+    
+//    @FocusState private var passwordInFocus: Bool
+    @State private var password = ""
+//    @FocusState private var usernameInFocus: Bool
+    @State private var username = ""
+    
+    @FocusState private var fieldInFocus: OnboardingField?
+    @State private var showPassword = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 30) {
+            TextField("Add your name here...", text: $username)
+                .focused($fieldInFocus, equals: .username)
+//                .focused($usernameInFocus)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.3))
+                .cornerRadius(10)
+                .padding(.horizontal)
+//                .onAppear {
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                        self.usernameInFocus = true
+//                    }
+//                }
+            
+//            SecureField(
+            
+            SecureField("Add your password here...", text: $password)
+                .focused($fieldInFocus, equals: .password)
+//                .focused($passwordInFocus)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.3))
+                .cornerRadius(10)
+                .padding(.horizontal)
+            
+            Button {
+                showPassword.toggle()
+            } label: {
+                Text(!showPassword ? "Show password:" : "Password is: \(password) Hide password!")
+            }
+
+            
+            Button("SIGN UP! 💣") {
+                let usernameIsValid = !username.isEmpty
+                let passwordIsValid = !password.isEmpty
+                if usernameIsValid && passwordIsValid {
+                    print("SIGN UP")
+                } else if usernameIsValid {
+                    fieldInFocus = .password
+//                    usernameInFocus = false
+//                    passwordInFocus = true
+                } else {
+                    fieldInFocus = .username
+//                    usernameInFocus = true
+//                    passwordInFocus = false
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+//            Button("TOGGLE FOCUS STATE!") {
+//                usernameInFocus.toggle()
+//            }
+//            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
@@ -19,3 +88,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
